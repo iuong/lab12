dc8580a420a46db2cc14e14eb13b1f6c23bd1148
Kenneth Luong <kennethluoong@gmail.com>
2023-11-17 11:37:36 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -33,6 +33,8 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	children.add(childNode);
+        	this.parent = childNode;
         }
         
         
@@ -41,14 +43,16 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
-                return this;
+            if (this.name == targetName) { return this; }
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+            	if (child.getNodeWithName(targetName) != null) {
+            		return child;
+            	}
             }
             
             // Not found anywhere.
@@ -62,10 +66,17 @@ public class FamilyTree
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
 
-            // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
-            // the nodes of a tree is like traversing a linked list. If that isn’t clear,
-            // draw a tree, mark any leaf node, and then mark its ancestors in order from
-            // recent to ancient. Expect a question about this on the final exam.
+            // ?????  Collect ancestors of this TreeNode into the array list.
+            // HINT: going up the nodes of a tree is like traversing a linked list.
+            // If that isn’t clear, draw a tree, mark any leaf node, and then mark its ancestors
+            // in order from recent to ancient.
+            // Expect a question about this on the final exam.
+            TreeNode curr = this;
+            
+            while (curr.parent != null) {
+            	curr = curr.parent;
+            	ancestors.add(curr);
+            }
 
             return ancestors;
         }
